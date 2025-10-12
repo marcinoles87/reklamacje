38fe1c450fe9ad4f2813b731440b3fcc3bf0cd21
marcinoles87 <marcinoles87@gmail.com>
2025-10-12 16:21:42 +0200

    add new components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DaneFirmy from './components/DaneFirmy';
 
 function App() {
+
+  const [daneFirmy , setDaneFirmy] = useState('')
+  const [daneZglaszajacego , setDaneZglaszajacego] = useState('')
+  const [data , setData ] = useState('')
+  const [towar , setTowar] = useState('')
+  const [zgloszenie , setZgloszenie] = useState('')
+  const [żadanie , setŻadanie] = useState('')
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wniosek do zgloszenia reklamacyjnego</h1>
+      <DaneFirmy></DaneFirmy>
+      
     </div>
   );
 }
---
+++ src/components/DaneFirmy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function DaneFirmy
+() {
+  return (
+    <div className='dane-firmy'>
+        <h1>Megapunkt 500 sp. z o.o</h1>
+
+    </div>
+  )
+}
+
+export default DaneFirmy
