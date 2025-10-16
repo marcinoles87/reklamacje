b928a5377c383c851a186826484abfa16fa1fa18
marcinoles87 <marcinoles87@gmail.com>
2025-10-16 16:02:39 +0200

    add new components

--- src/components/DaneProduktu.js
+++ src/components/DaneProduktu.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function DaneProduktu({setKod,setNazwa,setOpis}) {
   return (
-    <div className='reklamacja-container'>
+    <div className='dane-firmy'>
         <h2>Dane Produktu</h2>
-        <label for="kod">Kod : <input placeholder='kod produktu' onChange={ (e) => setKod(e.target.value)}></input></label>
-        <label for="nazwa">Nazwa : <input placeholder='nazwa produktu' onChange={ (e) => setNazwa(e.target.value)}></input></label>
-        <label for="opis_wady"><textarea placeholder='przyczyna reklmacji' onChange={ (e) => setOpis(e.target.value)}></textarea></label>
+        <p>Kod : <input placeholder='kod produktu' onChange={ (e) => setKod(e.target.value)}></input></p>
+        <label for="nazwa">Nazwa : <input placeholder='nazwa produktu' onChange={ (e) => setNazwa(e.target.value)}></input></p>
+        <label for="opis_wady"><textarea placeholder='opis wady / uszkodzenia' onChange={ (e) => setOpis(e.target.value)}></textarea></p>
     </div>
   )
 }
