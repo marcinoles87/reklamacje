5e33d0f43e3ab20606fbfaaf9d81989e2e19de99
marcinoles87 <marcinoles87@gmail.com>
2025-11-16 11:26:54 +0100

    add number count

--- src/components/DaneFirmy.js
+++ src/components/DaneFirmy.js
@@ -7,6 +7,7 @@ function DaneFirmy ({filia,setFilia,paragon,setParagon,sprzedawca,setSprzedawca}
     const dataDzis = new Date()
     const dataZgloszenia = `${dataDzis.getDate()}.${dataDzis.getMonth()+1}.${dataDzis.getFullYear()}`
 
+
     
   return (
     <div className='dane-firmy'>
@@ -15,8 +16,6 @@ function DaneFirmy ({filia,setFilia,paragon,setParagon,sprzedawca,setSprzedawca}
         <p>Paragon nr : <input type='text' onChange={ (e) =>setParagon(e.target.value)}></input></p>
         <p>Sporządził : <input type="text" onChange={ (e) =>setSprzedawca(e.target.value)}></input></p>
         <p>Wniosek sporządzono dnia : {dataZgloszenia}</p>
-
-
 
     </div>
   )
