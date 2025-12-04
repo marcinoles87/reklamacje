60f5da128ad05d944bff3becb67629a3462def71
marcinoles87 <marcinoles87@gmail.com>
2025-12-04 17:06:40 +0100

    update function

--- src/App.js
+++ src/App.js
@@ -217,7 +217,7 @@ function App() {
                         <p>Produkt : <span style={{color:'#00dd90ff',fontWeight:'bold'}}>{item.Kod} / {item.Nazwa} </span></p>
                         <p>Data zakupu : <span style={{color:'#00dd90ff',fontWeight:'bold'}}>{item.Zakup} </span></p>
                         <p>Paragon {item.Paragon}</p>
-                        <p>Żądanie : <span style={{color:'blue',borderBottom:'1px solid blue'}}>{item.Żadanie}</span> </p>
+                        <p>Żądanie : <span style={{color:'rgba(0, 223, 134, 1)'}}>{item.Żadanie}</span> </p>
                         <p>Opis wady : {item.OpisReklamacji}</p>
                         <p>Stan przedmiotu : {item.Wyglad}</p>
                         <p>Rozpatrzona : {item.Rozpatrzona ? <span style={{fontSize:'20px',color:'#00dd90ff',fontWeight:'bold'}}>TAK</span> : <span style={{color:'red'}}>'NIE'</span> }</p>
