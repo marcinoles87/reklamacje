04d8e1d0d106ff542deccc7a04cf7ad003e64509
marcinoles87 <marcinoles87@gmail.com>
2025-12-14 09:37:36 +0100

    update function print

--- src/components/Drukuj.js
+++ src/components/Drukuj.js
@@ -6,25 +6,28 @@ function Drukuj({item}) {
     <div className='druk-container'>
       Witamy w strefie druku reklamacji nr :
 
-      <div className='dane-client'>
-                          <h2>{item.OsobaZgłaszajaca}</h2>
-                          <i className="fa-solid fa-phone"></i> {item.Telefon}  <i className="fa-solid fa-envelope"></i> {item.Email}
-                      </div>
+            <div className='dane-client'>
+                <h2>{item.OsobaZgłaszajaca}</h2>
+                <i className="fa-solid fa-phone"></i> {item.Telefon}  <i className="fa-solid fa-envelope"></i> {item.Email}
+            </div>
+
+            <div>
+              <h2>Dane firmy</h2>
+              <p>Filia : {item.Filia}</p>
+               <p>Data sporządzenia reklamacji : {item.DataSporzadzenia}</p>
+              <p>Sporządził : {item.Podpisał}</p>
+            </div>
 
             <div className='dane-product'>
-
-
-
-
-                        <p>Produkt : <span style={{color:'#00dd90ff',fontWeight:'bold'}}>{item.Kod} / {item.Nazwa} </span></p>
-                        <p>Data zakupu : <span style={{color:'#00dd90ff',fontWeight:'bold'}}>{item.Zakup} </span></p>
-                        <p>Paragon {item.Paragon}</p>
-                        <p>Żądanie : <span style={{color:'rgba(0, 223, 134, 1)'}}>{item.Żadanie}</span> </p>
-                        <p>Opis wady : {item.OpisReklamacji}</p>
-                        <p>Stan przedmiotu : {item.Wyglad}</p>
-                        <p>Rozpatrzona : {item.Rozpatrzona ? <span style={{fontSize:'20px',color:'#00dd90ff',fontWeight:'bold'}}>TAK</span> : <span style={{color:'red'}}>'NIE'</span> }</p>
-
-                      </div>
+              <h2>Dane reklamacji</h2>
+              <p>Produkt : <span style={{color:'#00dd90ff',fontWeight:'bold'}}>{item.Kod} / {item.Nazwa} </span></p>
+              <p>Data zakupu : <span style={{color:'#00dd90ff',fontWeight:'bold'}}>{item.Zakup} </span></p>
+              <p>Paragon {item.Paragon}</p>
+              <p>Żądanie : <span style={{color:'rgba(0, 223, 134, 1)'}}>{item.Żadanie}</span> </p>
+              <p>Opis wady : {item.OpisReklamacji}</p>
+              <p>Stan przedmiotu : {item.Wyglad}</p>
+              <p>Rozpatrzona : {item.Rozpatrzona ? <span style={{fontSize:'20px',color:'#00dd90ff',fontWeight:'bold'}}>TAK</span> : <span style={{color:'red'}}>'NIE'</span> }</p>
+            </div>
 
 
       
